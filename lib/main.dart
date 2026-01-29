5a3121959d92ccb9d6fc161a83e8fad6ef3a86a7
Jordan Rodney <rodn0023@algonquinlive.com>
2026-01-29 13:19:49 -0500

    Completed Lab 3

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'labs/login.dart';
 
 void main() {
-  runApp(const Login());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -65,13 +66,11 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      if(_counter < 99.0)
-      _counter++;
+      if (_counter < 99.0) _counter++;
     });
   }
 
-  void setNewValue(double value)
-  {
+  void setNewValue(double value) {
     setState(() {
       _counter = value;
       myFontSize = value;
@@ -96,40 +95,264 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            Text('You have pushed the button this many times:',
-              style: TextStyle(fontSize: myFontSize)
-            ),
-            Text(
-              '$_counter',
-              style: TextStyle(fontSize: myFontSize),
-            ),
-            Slider(value: myFontSize, max:100.0, onChanged: setNewValue, min:0.0 )
-          ],
+      body: Container(
+        margin: const EdgeInsets.all(14.0),
+        child: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: SingleChildScrollView(child:
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "BROWSE CATEGORIES",
+                style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(28.0),
+                child: Text(
+                  "Not sure about exactly which recipe you're looking for? Do a search, or dive into our most popular categories.",
+                  style: TextStyle(fontSize: 15.0),
+                ),
+              ),
+              Text(
+                "BY MEAT",
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(28.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Stack(
+                      alignment: AlignmentDirectional.center,
+                      children: <Widget>[
+                        CircleAvatar(
+                          backgroundImage: AssetImage('images/lab3/beef.jpg'),
+                          radius: 40.0,
+                        ),
+                        Text(
+                          "BEEF",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: MediaQuery.of(context).size.width * 0.035,
+                            shadows: [
+                              Shadow(
+                                color: Colors.black,
+                                offset: Offset(0.0, 3.0),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Stack(
+                      alignment: AlignmentDirectional.center,
+                      children: <Widget>[
+                        CircleAvatar(
+                          backgroundImage: AssetImage(
+                            'images/lab3/chicken.jpg',
+                          ),
+                          radius: 40.0,
+                        ),
+                        Text(
+                          "CHICKEN",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: MediaQuery.of(context).size.width * 0.035,
+                            shadows: [
+                              Shadow(
+                                color: Colors.black,
+                                offset: Offset(0.0, 3.0),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Stack(
+                      alignment: AlignmentDirectional.center,
+                      children: <Widget>[
+                        CircleAvatar(
+                          backgroundImage: AssetImage('images/lab3/pork.jpg'),
+                          radius: 40.0,
+                        ),
+                        Text(
+                          "PORK",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: MediaQuery.of(context).size.width * 0.035,
+                            shadows: [
+                              Shadow(
+                                color: Colors.black,
+                                offset: Offset(0.0, 3.0),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Stack(
+                      alignment: AlignmentDirectional.center,
+                      children: <Widget>[
+                        CircleAvatar(
+                          backgroundImage: AssetImage(
+                            'images/lab3/seafood.jpg',
+                          ),
+                          radius: 40.0,
+                        ),
+                        Text(
+                          "SEAFOOD",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: MediaQuery.of(context).size.width * 0.035,
+                            shadows: [
+                              Shadow(
+                                color: Colors.black,
+                                offset: Offset(0.0, 3.0),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Text(
+                "BY COURSE",
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(28.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          backgroundImage: AssetImage(
+                            'images/lab3/main-dishes.jpg',
+                          ),
+                          radius: 40.0,
+                        ),
+                        SizedBox(height: 8),
+                        Text("Main Dishes", textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 12.0),),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          backgroundImage: AssetImage(
+                            'images/lab3/salad-recipes.jpg',
+                          ),
+                          radius: 40.0,
+                        ),
+                        SizedBox(height: 8),
+                        Text("Salad Recipes", textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 12.0),),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          backgroundImage: AssetImage(
+                            'images/lab3/side-dishes.jpg',
+                          ),
+                          radius: 40.0,
+                        ),
+                        SizedBox(height: 8),
+                        Text("Side Dishes", textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 12.0),),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          backgroundImage: AssetImage(
+                            'images/lab3/crockpot.jpg',
+                          ),
+                          radius: 40.0,
+                        ),
+                        SizedBox(height: 8),
+                        Text("Crockpot", textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 12.0),),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Text(
+                "BY DESSERT",
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(28.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          backgroundImage: AssetImage(
+                            'images/lab3/icecream.jpg',
+                          ),
+                          radius: 40.0,
+                        ),
+                        SizedBox(height: 8),
+                        Text("Ice Cream", textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 12.0),),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          backgroundImage: AssetImage(
+                            'images/lab3/brownies.jpg',
+                          ),
+                          radius: 40.0,
+                        ),
+                        SizedBox(height: 8),
+                        Text("Brownies", textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 12.0),),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          backgroundImage: AssetImage('images/lab3/pies.jpg'),
+                          radius: 40.0,
+                        ),
+                        SizedBox(height: 8),
+                        Text("Pies", textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 12.0),),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          backgroundImage: AssetImage(
+                            'images/lab3/cookies.jpg',
+                          ),
+                          radius: 40.0,
+                        ),
+                        SizedBox(height: 8),
+                        Text("Cookies", textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 12.0),),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          )
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
