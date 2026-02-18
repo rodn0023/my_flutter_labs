0e6c4e1230435dd1843123e2b4faa0f72d7d5865
Jordan Rodney <rodn0023@algonquinlive.com>
2026-02-18 18:00:35 -0500

    Completed Lab 6

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:my_flutter_labs/DataRepository.dart';
 import 'ProfilePage.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+void main() {
   runApp(const MyApp());
 }
 
@@ -39,129 +38,120 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  var password = 'placeholder';
-  late TextEditingController loginController;
-  late TextEditingController passwordController;
-  var imageSource = 'images/question-mark.png';
-  var imageDescription = 'a bundle of question marks';
+  late TextEditingController itemController;
+  late TextEditingController quantityController;
+
+  List<String> words =  [];
+  List<String> quantities =  [];
 
   @override
   void initState() {
     super.initState();
-    loginController = TextEditingController(text: DataRepository.loginName);
-    passwordController = TextEditingController(text: DataRepository.password);
 
-
-    DataRepository.init().then((done){
-
-      loginController.addListener(() {
-        DataRepository.loginName = loginController.text;
-        DataRepository.saveData();
-      });
-      passwordController.addListener(() {
-        DataRepository.password = passwordController.text;
-        DataRepository.saveData();
-      });
-
-      if (DataRepository.loginName.isNotEmpty || DataRepository.password.isNotEmpty) {
-        Future.delayed(Duration.zero, () {
-          final snackBar = SnackBar(
-            content: Text("User information successfully loaded!"),
-          );
-          ScaffoldMessenger.of(context).showSnackBar(snackBar);
-        });
-      }
-
-    });
-
+    itemController = TextEditingController();
+    quantityController = TextEditingController();
   }
 
   @override
   void dispose() {
-    loginController.dispose();
-    passwordController.dispose();
+    itemController.dispose();
+    quantityController.dispose();
     super.dispose();
   }
 
-  void saveInfo () async {
-
-    await DataRepository.saveData();
-    Navigator.of(context).pop();
-
-    if (passwordController.value.text == 'ASDF') {
-      DataRepository.loginName = loginController.text;
-      Navigator.pushNamed(  context,"/profilePage" );
-    }
-  }
-
-  void deleteInfo () async {
-    await DataRepository.deleteData();
-    Navigator.pop(context);
-  }
-
-  void buttonClicked() {
-    showDialog<String>(
+  void deleteAlert(int index) {
+    showDialog(
       context: context,
-      builder: (BuildContext context) => AlertDialog(
-        title: const Text('IMPORTANT'),
-        content: const Text('Do you wish to save your login information?'),
-        actions: <Widget>[
-          ElevatedButton( onPressed: saveInfo, child:  Text("Yes")  ),
-          ElevatedButton( onPressed: deleteInfo, child:  Text("No")  )
+      builder: (context) => AlertDialog(
+        title: Text("Remove an Item"),
+        content: Text("Do you wish to delete this item?"),
+        actions: [
+          ElevatedButton(
+            child: Text("NO"),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          ElevatedButton(
+            child: Text("YES"),
+            onPressed: () {
+              setState(() {
+                words.removeAt(index);
+                quantities.removeAt(index);
+                Navigator.of(context).pop();
+              });
+            },
+          )
         ],
       ),
     );
+  }
 
-    setState(() {
-      password = passwordController.value.text;
-      if (password == 'ASDF') {
-        imageSource = 'images/idea.png';
-        imageDescription = 'a glowing lightbulb';
-      } else {
-        imageSource = 'images/stop.png';
-        imageDescription = 'a stop sign';
-      }
-    });
+
+  Widget ListPage() {
+    return Column(
+      children: [
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: TextField(
+                controller: itemController,
+                decoration: const InputDecoration(
+                  hintText: "Type an item here",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Expanded(
+              child: TextField(
+                controller: quantityController,
+                decoration: const InputDecoration(
+                  hintText: "Type the quantity here",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  words.add(itemController.text);
+                  quantities.add(quantityController.text);
+                  itemController.clear();
+                  quantityController.clear();
+                });
+              },
+              child: const Text("Click here"),
+            ),
+          ],
+        ),
+        if (words.isEmpty)
+          Text("There are no items in the list."),
+        Expanded(
+          child: ListView.builder(
+            itemCount: words.length,
+            itemBuilder: (context, rowNum) {
+                return GestureDetector(child:
+                  Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("${rowNum + 1}. ${words[rowNum]}"),
+                    Text(" quantity: ${quantities[rowNum]}")
+                  ],
+                ),
+                onLongPress: () {deleteAlert(rowNum);});
+            },
+          ),
+        ),
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: .center,
-          children: [
-            TextField(controller: loginController,
-                decoration: InputDecoration(
-                    hintText:"John Smith",
-                    border: OutlineInputBorder(),
-                    labelText: "Login"
-                )),
-            TextField(controller: passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Password"
-                )),
-            ElevatedButton( onPressed: buttonClicked, child:  Text("Login")  ),
-            Semantics(label: imageDescription,
-                child: Image.asset(imageSource, height:300.0, width:300.0)
-            )
-          ],
-        ),
-      ),
+      body: ListPage()
     );
   }
 }
